43d4774ddd331e9233c7219ecd7f912cefa7e569
ohyoubee <dhdbql1022@gmail.com>
2024-01-11 21:15:17 +0900

    헬스케어

---
+++ src/main/java/com/example/gomdam/GomdamApplication.java
@@ -0,0 +1,13 @@
+package com.example.gomdam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GomdamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GomdamApplication.class, args);
+    }
+
+}
